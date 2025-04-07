7865b3bfd8910a22fe9a485134a3a36cb772be55
SeveriusRasmussen <144419314+SeveriusRasmussen@users.noreply.github.com>
2025-04-07 08:56:16 +0200

    First commit

---
+++ src/main/java/webclientgroup/villainroleai/VillainRoleAiApplication.java
@@ -0,0 +1,13 @@
+package webclientgroup.villainroleai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillainRoleAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillainRoleAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/webclientgroup/villainroleai/VillainRoleAiApplicationTests.java
@@ -0,0 +1,13 @@
+package webclientgroup.villainroleai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillainRoleAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
