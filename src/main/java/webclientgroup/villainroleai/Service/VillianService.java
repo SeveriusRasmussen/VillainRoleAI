b093e6d2924a6e71a6312b19f83423b08d3568c7
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-04-07 13:26:46 +0200

    noter til VillainService

--- src/main/java/webclientgroup/villainroleai/Service/VillianService.java
+++ src/main/java/webclientgroup/villainroleai/Service/VillianService.java
@@ -14,32 +14,36 @@ import java.util.List;
 @Service
 public class VillianService {
     @Value("${api.key}")
-    private String key;
+    private String key; //Secret API key for AI
 
-
-
+    //get response. Should take parameter with message content later. And hsould be renamed.
     public void getResponse() {
-        VillianRequest villianRequest = new VillianRequest();
         WebClient webClient = WebClient.create("https://api.mistral.ai");
 
-        villianRequest.setModel("mistral-small-latest");
-        villianRequest.setTemperature(1);
-        Message message = new Message("user", "Hello, I am a villain. How do i conquer the world?");
-        villianRequest.setMessages(List.of(message));
+        VillianRequest villianRequest = new VillianRequest();//new villain request. Contains messages.
 
+        villianRequest.setModel("mistral-small-latest"); //get AI model.
+        villianRequest.setTemperature(1); //how RANDOM should the answer be.
+        Message message = new Message("user", "Hello, I am a villain. How do i conquer the world?"); //set role and content of message.
+        //user: user message (eg. "what's the best cheese?")
+        //system: how should the AI behave. (eg. be a snobby parisian)
+
+        villianRequest.setMessages(List.of(message)); //set messages in villainRequest. List.of(message) creates new List<Message> containing message.
+
+        //post message, return AI response:
         webClient.post()
-                .uri("/v1/chat/completions")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + key)
+                .uri("/v1/chat/completions")//specify endpoint.
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + key) //set credentials
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(villianRequest)
-                .retrieve()
-                .bodyToMono(VillianReponse.class)
-                .subscribe(response -> {
-                    String content = response.getChoices().get(0).getMessage().getContent();
-                    System.out.println("AI: " + content);
+                .bodyValue(villianRequest)//create JSON from villainRequest.
+                .retrieve()//get response
+                .bodyToMono(VillianReponse.class)//create mono from response.
+                .subscribe(response -> { //??
+                    String content = response.getChoices().get(0).getMessage().getContent(); //get content from response.
+                    System.out.println("AI: " + content); //print content.
                 });
 
-        System.out.println(villianRequest);
+        //System.out.println(villianRequest); //
     }
 }
 
