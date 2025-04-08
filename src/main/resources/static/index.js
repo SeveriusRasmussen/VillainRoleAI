4efc123dbe1db2ee63b59325afbcfdbe7c0ca466
MH020 <166133291+MH020@users.noreply.github.com>
2025-04-08 09:54:01 +0200

    oprettet til fetch

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,4 +1,21 @@
-console.log("javascript virker fint ")
+console.log("javascript virker fint ");
+const url = "/villians";
+
+function fetchReponse() {
+    fetch(`${url}/charecterSheet/all`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("FEJL NUMBNUTS");
+            }
+            return response.json();
+        })
+        .then(characterSheets => {
+
+        })
+        .catch(err => {
+            console.error("Der opstod en fejl:", err);
+        });
+}
 
 function display() {
     const textbox = document.createElement("div");
@@ -6,11 +23,32 @@ function display() {
 
     textbox.innerHTML = `
     <input type="text" id="AInput" placeholder="Ask me anything..."<input>
-    <button id="AButton">Ask</button>
-    <div id="AResponse"></div> 
-    
+    <button type="button" id="askButton">Ask</button>
     `;
     document.body.appendChild(textbox);
+    const askButton = document.getElementById("askButton");
+    const input = document.getElementById("AInput");
+    askButton.addEventListener("click", () => {
+        const questionData = document.querySelector("#AInput").value;
+
+        //post ticket object from html form.
+        fetch(`${url}/send question`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept':'application/json'
+            },
+            body: JSON.stringify(questionData) //create json file form movieData.
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log('Response Success:', result);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+    });
 
 }
 display();
