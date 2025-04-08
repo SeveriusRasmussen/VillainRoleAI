659ced3d803b081eb0a4da3ed4ef7aaca639e2b0
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-04-08 14:18:56 +0200

    fox controller

--- src/main/java/webclientgroup/villainroleai/controller/AIController.java
+++ src/main/java/webclientgroup/villainroleai/controller/AIController.java
@@ -5,19 +5,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import webclientgroup.villainroleai.aiapi.VillainResponse;
+import webclientgroup.villainroleai.foxapi.Fox;
+import webclientgroup.villainroleai.service.FoxService;
 import webclientgroup.villainroleai.service.VillainService;
 
 @RestController
 public class AIController {
     private final VillainService villainService;
+    private final FoxService foxService;
 
-    public AIController(VillainService villainService) {
+    public AIController(VillainService villainService, FoxService foxService) {
         this.villainService = villainService;
+        this.foxService = foxService;
     }
 
     //send RequestParam question to AI and return AI response.
     @GetMapping("/villain")
     public Mono<VillainResponse> getVillain(@RequestParam String question) {
         return villainService.getResponse(question);
+    }
+
+    @GetMapping("/fox")
+    public Mono<Fox> getFox(){
+        return foxService.getRandomFox();
     }
 }
--- src/main/java/webclientgroup/villainroleai/service/FoxService.java
+++ src/main/java/webclientgroup/villainroleai/service/FoxService.java
@@ -1,16 +1,18 @@
 package webclientgroup.villainroleai.service;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import webclientgroup.villainroleai.foxapi.Fox;
 
+@Service
 public class FoxService {
     public Mono<Fox> getRandomFox(){
 
         WebClient webClient = WebClient.create("https://randomfox.ca");
 
         Mono<Fox> monoFox = webClient.get().
-                uri("/floof").
+                uri("/floof/").
                 retrieve().
                 bodyToMono(Fox.class);
 
